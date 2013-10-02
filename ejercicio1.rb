50afb05c1124bedc3e6f50158c63f50ad0c72543
Matias Szeinfeld <matinger@gmail.com>
2013-10-01 22:14:27 -0300

    Actualizo ejercicios

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,128 +1,128 @@
 module Walk
-    def walk
-    	puts "#{self.to_s} is walking"
-    end
+  def walk
+    puts "#{self.to_s} is walking"
+  end
 end
 
 module Swim
-    def swim
-        puts "#{self.to_s} is swimming"
-    end
+  def swim
+    puts "#{self.to_s} is swimming"
+  end
 end
 
 module Speak
-    def speak
-        puts "#{self.to_s}} is speaking"
-    end
+  def speak
+    puts "#{self.to_s}} is speaking"
+  end
 end
 
 module Bark
-    def bark
-        puts "#{self.to_s}} is barking"
-    end
+  def bark
+    puts "#{self.to_s}} is barking"
+  end
 end
 
 module Fly
-    def fly
-        puts "#{self.to_s}} is flying"
-    end
+  def fly
+    puts "#{self.to_s}} is flying"
+  end
 end
 
 module Displace
-    def displace
-        puts "#{self.to_s}} is shifting"
-    end
+  def displace
+    puts "#{self.to_s}} is shifting"
+  end
 end
 
 module Jump
-    def jump
-        puts "#{self.to_s}} is jumping"
-    end
+  def jump
+    puts "#{self.to_s}} is jumping"
+  end
 end
 
 module Press
-    def press
-        puts "#{self.to_s}} presses"
-    end
+  def press
+    puts "#{self.to_s}} presses"
+  end
 end
 
 module Bite
-    def bite
-        puts "#{self.to_s}} is biting"
-    end
+  def bite
+    puts "#{self.to_s}} is biting"
+  end
 end
 
-class Animal
+class Animal; end
 
-end
+class Mammal < Animal; end
 
-class Mammal < Animal
+class Bird < Animal; end
 
-end
+class Reptile < Animal; end
 
-class Bird < Animal
+class Amphibian < Animal; end
 
-end
-
-class Reptile < Animal
-
-end
-
-class Amphibian < Animal
-
-end
-
-class Fish < Animal
-
-end
+class Fish < Animal; end
 
 class Man < Mammal
-    include Walk
-    include Swim
-    include Speak
-
+  include Walk
+  include Swim
+  include Speak
 end
 
 class Dog < Mammal
-    include Walk
-    include Bark
+  include Walk
+  include Bark
 end
 
 class Whale < Mammal
-    include Swim
+  include Swim
 end
 
 class Eagle < Bird
-    include Walk
-    include Fly
+  include Walk
+  include Fly
 end
 
 class Penguin < Bird
-    include Walk
-    include Swim
+  include Walk
+  include Swim
 end
 
 class Crocodile < Reptile
-    include Walk
-    include Swim
+  include Walk
+  include Swim
 end
 
 class Boa < Reptile
-    include Displace
-    include Press
+  include Displace
+  include Press
 end
 
 class Cobra < Reptile
-    include Displace
-    include Bite
+  include Displace
+  include Bite
 end
 
 class Toad < Amphibian
-    include Swim
-    include Jump
+  include Swim
+  include Jump
 end
 
 class Shark < Fish
-    include Swim
+  include Swim
 end
 
+#Testing
+h = Man.new
+h.walk
+h.speak
+
+t = Toad.new
+t.jump
+
+e = Eagle.new
+e.fly
+
+d = Dog.new
+d.fly
--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,44 +1,46 @@
 class Product
-    attr_accessor :code, :name, :price
-
-    def initialize(code, name, price)
-        @code = code
-        @price = Float(price)
-        @name = name
-    end
-
-    
+  attr_accessor :code, :name, :price
+  
+  def initialize(code, name, price)
+    @code = code
+    @price = Float(price)
+    @name = name
+  end   
 end
 
 class Purchase
-    attr_accessor :products
+  attr_accessor :products, :total
+  
+  def initialize
+    @total = 0.0
+    @products = []
+  end
+  
+  def add(product)
+    @total += product.price
+    @products << product
+  end
+  
+  def delete(product)
+    @total -= product.price
+    @products.delete(product)
+  end
 
-    def initialize
-        @products = []
+  def total    
+    @total
+  end
+  
+  def cleandiscount
+    @total = 0.0
+    @products.each do |product| 
+        @total += product.price
     end
-
-    def add(product)
-        @products << product
-    end
-
-    def delete(product)
-	@products.delete(product)
-    end
-
-    def total
-        total = 0.0
-        @products.each do |product| 
-            sum += product.price
-        end
-        total
-    end
-
-    def apply_discount
-        yield(self)
-    end
+  end
+  
+  def apply_discount
+    yield(self)
+  end
 end
-
-
 
 p1 = Product.new '01', 'Martillo', '50'
 p2 = Product.new '02', 'Clavos x 100', '10'
@@ -47,14 +49,49 @@ p = Purchase.new
 p.add p1
 p.add p2
 
-p.total
+p p.total
 # => 60
 
 # Descuento de 10% en tu compra si llevás algún martillo!
 p.apply_discount do |purchase|
-  hammers = purchase.products.collect { |product| product.code == '01'  }
+  hammers = purchase.products.collect { |product| product.code == '01' }
   purchase.total -= purchase.total * 0.1 if hammers.any?
 end
 
-p.total
+p p.total
 # => 54.0
+
+# Descuento de 20% en tu compra si tiene más de 10 productos!
+p.apply_discount do |purchase|
+  purchase.total *= 0.8 if purchase.products.size > 10
+end
+
+p p.total
+# => 54.0
+
+3.times do 
+  p.add p1
+end
+
+p.cleandiscount
+
+#Si la lista de productos tiene más de 3 productos iguales, cada 3 paga 2
+p.apply_discount do |purchase|
+  prices = {}
+  counts = Hash.new(0)
+
+  purchase.products.each do |product|
+    counts[product.code] += 1
+    prices[product.code] ||= product.price
+  end
+  counts.each do |key, value|
+    if value > 3
+      productsdiscount = value.div 3
+      purchase.total -= productsdiscount * prices[key]
+    end
+  end 
+end
+
+p p.total
+# => 160.0
+
